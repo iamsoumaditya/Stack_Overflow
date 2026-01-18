222a6774aa52bdd3fba520e1db61b8521264854a
iamsoumaditya <iamsoumadityaroy@gmail.com>
2026-01-19 04:11:29 +0530

    Refactor error handling in user profile updates and implement debounce for fetching latest questions

--- src/app/(auth)/verify/page.tsx
+++ src/app/(auth)/verify/page.tsx
@@ -2,38 +2,63 @@
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState, use } from "react";
 import { account } from "@/src/models/client/config";
+import { Bounce, ToastContainer, toast } from "react-toastify";
+import { useTheme } from "next-themes";
 
-export default function VerifyPage() {
+export default function VerifyPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ secret: string; userId: string }>;
+}) {
+  const { resolvedTheme } = useTheme();
   const router = useRouter();
   const [error, setError] = useState("");
   const [isloading, setIsLoading] = useState(false);
   const [isVerified, setIsVerified] = useState(false);
-  const searchParams = useSearchParams();
-
-  const secret = searchParams.get("secret");
-  const userId = searchParams.get("userId");
+  const params = use(searchParams);
 
   useEffect(() => {
     setIsLoading(true);
-    if (!userId || !secret) {
-        setError("Unable to find the credentials!");
-        return;
+    if (!params.userId || !params.secret) {
+      setError("Unable to find the credentials!");
+      return;
     }
     setError("");
     const res = account.updateVerification({
-      userId,
-      secret,
+      userId: params.userId,
+      secret: params.secret,
     });
     res.then(
       (res: any) => {
-        console.log(res);
+        toast.success("Verified successfully!!", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: resolvedTheme === "dark" ? "dark" : "light",
+          transition: Bounce,
+        });
         setIsLoading(false);
         setIsVerified(true);
       },
       (err: any) => {
-        setIsLoading(false)
+        toast.error(err.message || "Verification failed", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: resolvedTheme === "dark" ? "dark" : "light",
+          transition: Bounce,
+        });
+        setIsLoading(false);
         setError(err);
-      }
+      },
     );
     if (isVerified) {
       router.push("/");
@@ -42,6 +67,19 @@ export default function VerifyPage() {
 
   return (
     <div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme={resolvedTheme}
+        transition={Bounce}
+      />
       <p className="mt-8 text-center text-2xl text-cyan-300 dark:text-cyan-400">
         Email Verification Page
       </p>
